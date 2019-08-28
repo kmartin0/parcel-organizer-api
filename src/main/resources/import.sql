1cb50e25311d444eff58ef7f05c5e07916984fef
Kevin Martin <kevin.martin@live.nl>
2019-08-28 13:06:58 +0200

    Fix hibernate identity starting at index zero returning unique constraint error

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,20 +7,20 @@ VALUES ('parcel-organizer-android', '$2a$12$o3dmbF3ElqPL1ApJ.9R/Qu7cVBMyV8pn80.H
         'ROLE_CLIENT', 50000, 256300);
 
 -- #Password = pass
-INSERT INTO "user"(id, password, name, email)
-VALUES (1, '$2a$12$Uv6GCKwJk7SaEPpy/h/dM.Qf4/BE5OTLc.31cPYrPc/Sl/LhUO1GO', 'kev', 'kev@live.nl'),
-       (2, '$2a$12$Uv6GCKwJk7SaEPpy/h/dM.Qf4/BE5OTLc.31cPYrPc/Sl/LhUO1GO', 'Kevin', 'kevin@live.nl'),
-       (3, '$2a$12$Uv6GCKwJk7SaEPpy/h/dM.Qf4/BE5OTLc.31cPYrPc/Sl/LhUO1GO', 'K. Martin', 'kmartin0@live.nl'),
-       (4, '$2a$12$Uv6GCKwJk7SaEPpy/h/dM.Qf4/BE5OTLc.31cPYrPc/Sl/LhUO1GO', 'k', 'k@live.nl');
+INSERT INTO "user"(password, name, email)
+VALUES ('$2a$12$Uv6GCKwJk7SaEPpy/h/dM.Qf4/BE5OTLc.31cPYrPc/Sl/LhUO1GO', 'kev', 'kev@live.nl'),
+       ('$2a$12$Uv6GCKwJk7SaEPpy/h/dM.Qf4/BE5OTLc.31cPYrPc/Sl/LhUO1GO', 'Kevin', 'kevin@live.nl'),
+       ('$2a$12$Uv6GCKwJk7SaEPpy/h/dM.Qf4/BE5OTLc.31cPYrPc/Sl/LhUO1GO', 'K. Martin', 'kmartin0@live.nl'),
+       ('$2a$12$Uv6GCKwJk7SaEPpy/h/dM.Qf4/BE5OTLc.31cPYrPc/Sl/LhUO1GO', 'k', 'k@live.nl');
 
 
-INSERT INTO parcel_status(id, status)
-VALUES (1, 'ORDERED'),
-       (2, 'SENT'),
-       (3, 'DELIVERED');
+INSERT INTO parcel_status(status)
+VALUES ('ORDERED'),
+       ('SENT'),
+       ('DELIVERED');
 
 
 INSERT INTO parcel(courier, last_updated, sender, title, tracking_url, parcel_status, "user")
-VALUES ('dpd', '2019-07-10 17:41:36', 'Bol', 'Iphone', 'dpd.com', 1, 4),
-       ('postnl', '2019-07-10 17:41:36', 'Zalando', 'Shoes', 'postnl.nl', 3, 4),
-       ('dhl', '2019-07-10 17:41:36', 'Wehkamp', 'Clothes', 'dhl.com', 2, 4);
+VALUES ('dpd', '2019-07-10 17:41:36', 'Bol', 'Iphone', 'dpd.com', 1, 3),
+       ('postnl', '2019-07-10 17:41:36', 'Zalando', 'Shoes', 'postnl.nl', 3, 3),
+       ('dhl', '2019-07-10 17:41:36', 'Wehkamp', 'Clothes', 'dhl.com', 2, 3);
